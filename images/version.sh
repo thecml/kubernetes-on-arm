0756b6f46d7dd029d5b4ad3911df5d65655369b3
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2015-08-01 23:55:07 +0300

    uppdaterade k8s version till 1.0.1, så det inte buggar

--- images/version.sh
+++ images/version.sh
@@ -1,7 +1,7 @@
 # Specify which versions the binaries should have
 
 # Two options, 'latest', or a version number like '1.0.1'
-K8S_VERSION='0.21'
+K8S_VERSION='1.0.1'
 ETCD_VERSION='2.0.4'
 FLANNEL_VERSION='0.5.2'
 GO_VERSION='1.4.2'
