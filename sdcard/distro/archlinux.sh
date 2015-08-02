0b99084b60ee283df57e8e091cb55a9fac6c7545
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2015-08-02 16:53:29 +0300

    fixar ett f för mycket i tar

--- sdcard/distro/archlinux.sh
+++ sdcard/distro/archlinux.sh
@@ -1,6 +1,6 @@
 dlandextract(){
 	# Download 
-	curl -sSL -k http://archlinuxarm.org/os/ArchLinuxARM-${MACHINENAME}-latest.tar.gz | tar -zxf -C $ROOT
+	curl -sSL -k http://archlinuxarm.org/os/ArchLinuxARM-${MACHINENAME}-latest.tar.gz | tar -xz -C $ROOT
 }
 
 populateboot(){
