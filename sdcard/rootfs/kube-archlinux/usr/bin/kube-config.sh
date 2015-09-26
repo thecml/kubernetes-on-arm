c1fc4914633c59886b381306c9c1d4ccdc177e11
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2015-09-26 23:41:36 +0300

    tar bort arch linux update

--- sdcard/rootfs/kube-archlinux/usr/bin/kube-config.sh
+++ sdcard/rootfs/kube-archlinux/usr/bin/kube-config.sh
@@ -31,7 +31,7 @@ install(){
 
 	# Could these two pacman commands be combined?
 	echo "Updating the system..."
-	time pacman -Syu --noconfirm
+	#time pacman -Syu --noconfirm
 
 	echo "Now were going to install some packages"
 	pacman -S docker git make nmap --noconfirm
