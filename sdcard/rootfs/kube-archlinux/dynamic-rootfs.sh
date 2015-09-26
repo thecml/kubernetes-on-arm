686439b463b74cd2359278da7e621012fd51df2a
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2015-09-26 21:25:16 +0300

    encapsulate the code in dynamic rootfs to a function

--- sdcard/rootfs/kube-archlinux/dynamic-rootfs.sh
+++ sdcard/rootfs/kube-archlinux/dynamic-rootfs.sh
@@ -1,13 +1,15 @@
-# Allow ssh connections by root to this machine
-echo "PermitRootLogin yes" >> $ROOT/etc/ssh/sshd_config
+rootfs(){
+	# Allow ssh connections by root to this machine
+	echo "PermitRootLogin yes" >> $ROOT/etc/ssh/sshd_config
 
-# Copy current source
-cp -r $PROJROOT/images $ROOT/etc/kubernetes/
+	# Copy current source
+	cp -r $PROJROOT/images $ROOT/etc/kubernetes/
 
-# If kubectl exists, include in rootfs
-if [[ -f $PROJROOT/k8s/_bin/latest/kubectl ]]; then
-	cp $PROJROOT/k8s/_bin/latest/kubectl $ROOT/usr/bin
-fi
+	# If kubectl exists, include in rootfs
+	if [[ -f $PROJROOT/k8s/_bin/latest/kubectl ]]; then
+		cp $PROJROOT/k8s/_bin/latest/kubectl $ROOT/usr/bin
+	fi
 
-# Remove the .sh
-mv $ROOT/usr/bin/kube-config.sh $ROOT/usr/bin/kube-config
+	# Remove the .sh
+	mv $ROOT/usr/bin/kube-config.sh $ROOT/usr/bin/kube-config
+}
