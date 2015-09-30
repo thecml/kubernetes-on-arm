d38518a77352f86022d4c9253f669a1e388cd7bf
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2015-09-30 22:16:06 +0300

    Removed some old scripts

--- old/archlinux/archlinux.sh
+++
@@ -1,9 +0,0 @@
-initos(){
-	# Download 
-	curl -sSL -k http://archlinuxarm.org/os/ArchLinuxARM-${MACHINENAME}-latest.tar.gz | tar -xz -C $ROOT
-
-	sync
-
-	# Move /boot to separate partition
-	mv $ROOT/boot/* $BOOT
-}
