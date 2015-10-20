547a40456f9159e2d8aa98c933f63a20a9ab5335
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2015-10-20 16:34:49 +0300

    Add task to version.sh

--- images/version.sh
+++ images/version.sh
@@ -1,6 +1,7 @@
 # Specify which versions the binaries should have
 
 # Two options, a branch like 'master' or a version tag e. g. 'v1.0.1'
+# Todo: eventually include the download paths here also
 
 ## GITHUB PROJECTS ##
 # These projects are downloaded from "github.com/user/repo/archive/$VERSION.tar.gz"
@@ -10,11 +11,4 @@ FLANNEL_VERSION='v0.5.3'
 REGISTRY_VERSION='v2.1.1'
 
 ## OTHER PROJECTS ##
-
 GO_VERSION='1.4.2'
-
-
-# WORKING VERSIONS
-#K8S_VERSION='v1.0.1'
-#ETCD_VERSION='v2.0.4'
-#FLANNEL_VERSION='v0.5.3'
