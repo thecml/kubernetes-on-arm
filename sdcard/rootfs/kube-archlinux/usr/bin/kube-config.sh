13c6ead0c4d28302f992245feb58cfa34cc192a6
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2015-10-24 22:17:04 +0300

    Lots of changes and bugfixes in kube-config. Some cleanup

--- sdcard/rootfs/kube-archlinux/usr/bin/kube-config.sh
+++ sdcard/rootfs/kube-archlinux/usr/bin/kube-config.sh
@@ -9,16 +9,28 @@ ADDONS_DIR=$KUBERNETES_DIR/addons
 KUBERNETES_CONFIG=$KUBERNETES_DIR/k8s.conf
 PROJECT_SOURCE=$KUBERNETES_DIR/source
 K8S_PREFIX="kubernetesonarm"
+
+# The images that are required
 REQUIRED_MASTER_IMAGES=("$K8S_PREFIX/flannel $K8S_PREFIX/etcd $K8S_PREFIX/hyperkube $K8S_PREFIX/pause")
 REQUIRED_WORKER_IMAGES=("$K8S_PREFIX/flannel $K8S_PREFIX/hyperkube $K8S_PREFIX/pause")
-REQUIRED_ADDON_IMAGES=("$K8S_PREFIX/skydns $K8S_PREFIX/kube2sky $K8S_PREFIX/exechealthz $K8S_PREFIX/registry")
+REQUIRED_ADDON_IMAGES=("$K8S_PREFIX/skydns $K8S_PREFIX/kube2sky $K8S_PREFIX/exechealthz $K8S_PREFIX/registry $K8S_PREFIX/kube-ui")
 
+DEFAULT_TIMEZONE="Europe/Helsinki"
+DEFAULT_HOSTNAME="kubepi"
+
+# If the config doesn't exist, create
+if [[ ! -f $KUBERNETES_CONFIG ]]; then
+	echo "K8S_MASTER_IP=127.0.0.1" > $KUBERNETES_CONFIG
+fi
+
+# Source the config
+source $KUBERNETES_CONFIG
 
 usage(){
 	cat <<EOF
 Welcome to kube-config!
 
-With this utility, you can setup kubernetes on ARM!
+With this utility, you can setup Kubernetes on ARM!
 
 Usage: 
 	kube-config install - Installs docker and makes your board ready for kubernetes
@@ -35,13 +47,16 @@ Usage:
 			- luxas/raspbian: A Raspbian base image. Based on resin/rpi-raspbian.
 
 	kube-config enable-master - Enable the master services and then kubernetes is ready to use
+		- FYI, etcd data will be stored in the /var/lib/etcd directory. Backup that directory if you have important data.
 	kube-config enable-worker - Enable the worker services and then kubernetes has a new node
 	kube-config enable-addon [addon] - Enable an addon
 		- Currently defined addons
 				- dns: Makes all services accessible via DNS
 				- registry: Makes a central docker registry
+				- kube-ui: Sets up an UI for Kubernetes. Experimental. Doesn't show anything useful just now, waiting for upstream.
 
-	kube-config disable-machine - Disable Kubernetes on this machine, reverting the enable actions, useful if something went wrong
+	kube-config disable-node - Disable Kubernetes on this node, reverting the enable actions, useful if something went wrong
+	kube-config disable - Synonym to disable-machine
 	kube-config disable-addon [addon] - Disable an addon, not the whole cluster
 	
 	kube-config delete-data - Clean the /var/lib/etcd directory, where all master data is stored
@@ -50,8 +65,6 @@ Usage:
 	kube-config help - Display this help
 EOF
 }
-
-
 
 
 install(){
@@ -66,18 +79,17 @@ install(){
 	# Create a symlink to the dropin location, so docker will use overlay
 	dropins-enable-overlay
 
-	
 	# Enable the system-docker service and restart both
 	systemctl enable system-docker docker
 	systemctl restart system-docker docker
 
 	echo "Set the timezone"
 	if [[ -z $TIMEZONE ]]; then
-		read -p "What timezone should be set? Defaults to Europe/Helsinki. " timezoneanswer
+		read -p "Which timezone should be set? Defaults to $DEFAULT_TIMEZONE. " timezoneanswer
 
 		# Defaults to Helsinki
 		if [[ -z $timezoneanswer ]]; then
-			timedatectl set-timezone Europe/Helsinki
+			timedatectl set-timezone $DEFAULT_TIMEZONE
 		else
 			timedatectl set-timezone $timezoneanswer
 		fi
@@ -101,11 +113,11 @@ install(){
 
 	# Only set if its specified
 	if [[ -z $NEW_HOSTNAME ]]; then
-		read -p "What hostname do you want? Defaults to kubepi. " hostnameanswer
+		read -p "What hostname do you want? Defaults to $DEFAULT_HOSTNAME. " hostnameanswer
 
 		# Defaults to kubepi
 		if [[ -z $hostnameanswer ]]; then
-			hostnamectl set-hostname kubepi
+			hostnamectl set-hostname $DEFAULT_HOSTNAME
 		else
 			hostnamectl set-hostname $hostnameanswer
 		fi
@@ -113,11 +125,15 @@ install(){
 	else
 		hostnamectl set-hostname $NEW_HOSTNAME
 	fi
+
+	# Reboot?
 	if [[ -z $REBOOT ]]; then
-		read -p "Do you want to reboot now? A reboot is required for Docker to function. [Y/n] " rebootanswer
+		read -p "Do you want to reboot now? A reboot is required for Docker to function. Y is default. [Y/n] " rebootanswer
 
 		case $rebootanswer in
-			[yY]*)
+			[nN]*)
+				echo "Done.";;
+			*)
 				reboot;;
 		esac
 	else [[ $REBOOT = 1 ]]
@@ -126,7 +142,7 @@ install(){
 }
 
 swap(){
-	echo "Make an 1GB swapfile, NOTE: it takes up precious sd card space"
+	echo "Make an 1GB swapfile, NOTE: it takes up precious SD Card space"
 
 	# Check that the swapfile doesn't already exist
 	if [[ ! -f /swapfile ]]; then
@@ -146,8 +162,11 @@ EOF
 	fi
 }
 
+### --------------------------------- HELPERS -----------------------------------
+
+# A forwarder to the build script in the repo
 build(){
-	/etc/kubernetes/source/images/build.sh "$@"
+	$PROJECT_SOURCE/images/build.sh "$@"
 }
 
 # Remove all docker dropins. They are symlinks, so it doesn't matter
@@ -184,7 +203,8 @@ require-images(){
 		if [[ -z $(docker images | grep "$IMAGE") ]]; then
 
 			echo "Can't spin up Kubernetes without these images: $@"
-			pull-images
+			pull-images "$@"
+			break
 		fi
 	done
 }
@@ -192,24 +212,34 @@ require-images(){
 pull-images(){
 
 	echo "Tries to pull them instead."
-	# Try to pull the images
-	docker pull $@
 
-	if [[ -z $(docker images | grep "$IMAGE") ]]; then
-		echo "Pull failed"
-		exit 1
-	fi
+	# For each 
+	for IMAGE in "$@"; do
 
-	
+		if [[ -z $(docker images | grep "$IMAGE") ]]; then
+
+			# Try to pull the image
+			docker pull $IMAGE
+
+			# Double-check if it's here
+			if [[ -z $(docker images | grep "$IMAGE") ]]; then
+
+				echo "Pull failed. Try to pull these images yourself: $@"
+				exit
+			fi
+		fi
+	done
+
+	# If kubectl doesn't exist, download from github
 	if [[ ! -f /usr/bin/kubectl ]]; then
 		echo "Downloading kubectl..."
 		curl https://github.com/luxas/kubernetes-on-arm/releases/download/v0.5.5/kubectl > /usr/bin/kubectl
 	fi
 }
 
-# Load the images which is necessary to system-docker 
+# Load an image to system-docker
 load-to-system-docker(){
-	# If they doesn't exist, load them from docker
+	# If they doesn't exist, load them from main docker
 	if [[ -z $(docker -H unix:///var/run/system-docker.sock images | grep "$1") ]]; then
 		echo "Copying $1 to system-docker"
 		docker save $1 | docker -H unix:///var/run/system-docker.sock load
@@ -228,7 +258,29 @@ get-node-type(){
 	fi
 }
 
+# Is kubernetes enabled?
+is-active(){
+	if [[ get-node-type != "" ]]; then
+		return 1;
+	else 
+		return 0;
+	fi
+}
+
+checkformaster(){
+	# If ping doesn't return unknown, its OK
+	if [[ -z $(ping -c1 $K8S_MASTER_IP | grep unknown) ]]; then
+		echo "OK"
+	fi
+}
+
+# ----------------------------------------------- MAIN -------------------------------------------
+
 start-master(){
+
+	# Disable some (already running?) services
+	disable >/dev/null
+	sleep 1
 
 	# Require these images to be present
 	require-images ${REQUIRED_MASTER_IMAGES[@]}
@@ -248,7 +300,7 @@ start-master(){
 	# Wait for etcd and flannel
 	sleep 5
 
-	# Create a symlink to the dropin location, so docker will use flannel
+	# Create a symlink to the dropin location, so docker will use flannel. Also starts docker
 	dropins-enable-flannel
 
 	# Wait for docker to come up
@@ -265,11 +317,15 @@ start-master(){
 
 start-worker(){
 
+	# Disable some (already running?) services
+	disable >/dev/null
+	sleep 1
+
 	# Check if we have a connection
-	if [[ $(checkformaster) != "OK" ]]; then
+	if [[ $K8S_MASTER_IP == "127.0.0.1" || $(checkformaster) != "OK" ]]; then
 
 		# Ask for the ip
-		read -p "What is the master ip? It isn't specified or reachable at the moment." masteripanswer
+		read -p "What is the Master IP? It isn't specified or reachable at the moment. " masteripanswer
 
 		# Required
 		if [[ -z $masteripanswer ]]; then
@@ -277,11 +333,13 @@ start-worker(){
 			exit 1
 		else
 			echo "K8S_MASTER_IP=$masteripanswer" > $KUBERNETES_CONFIG
+			K8S_MASTER_IP=$masteripanswer
 		fi
 
 		# Check again and fail if it's not working now either
 		if [[ $(checkformaster) != "OK" ]]; then
-			echo "The master ip you provided isn't reachable. Exiting..."
+			echo "The Master IP you provided isn't reachable. Exiting..."
+			exit
 		fi
 	fi
 
@@ -313,38 +371,46 @@ start-worker(){
 	echo "Worker Kubernetes services enabled"
 }
 
-start(){
-	if [[ get-node-type != "" ]]; then
+start-addon(){
+	if [[ is-active ]]; then
 
+		# The addon images are required
+		# Todo, make faster
 		require-images ${REQUIRED_ADDON_IMAGES[@]}
 
-		local SVC=start-$1
+		# Invoke an optional addon function
+		case $1 in
+			'dns') addon-dns;;
 
-		# Invoke the function
-		$($SVC >/dev/null)
+			# For each file in the addon dir, kubectl create
+			*) 	for FILE in $ADDONS_DIR/$1/*.yaml; do
+					kubectl create -f $FILE
+				done;;
+		esac
+			
 
-		echo "Started $1"
+		echo "Started addon: $1"
 	else
 		echo "Kubernetes is not running!"
 	fi
 }
 
-stop(){
-	if [[ get-node-type != "" ]]; then
-		local SVC=stop-$1
+stop-addon(){
+	if [[ is-active ]]; then
 
-		# Invoke the function
-		$($SVC >/dev/null)
+		# Stop all services
+		for FILE in $ADDONS_DIR/$1/*.yaml; do
+			kubectl delete -f $FILE
+		done
 
-		echo "Stopped $1"
+		echo "Stopped addon: $1"
 	else
 		echo "Kubernetes is not running!"
 	fi
 }
 
-start-dns(){
-	source $KUBERNETES_CONFIG
-
+# Start the dns customized
+addon-dns(){
 	# Replace the KUBEMASTER placeholder with the master ip temporary, until service accounts is in place
 	if [[ $K8S_MASTER_IP == "127.0.0.1" ]]; then
 		K8S_MASTER_IP=$(hostname -i | awk '{print $1}')
@@ -353,19 +419,6 @@ start-dns(){
 	sed -e "s@KUBEMASTER@http://$K8S_MASTER_IP:8080@" $ADDONS_DIR/dns/dns-rc.yaml | kubectl create -f -
 	kubectl create -f $ADDONS_DIR/dns/dns-svc.yaml
 }
-stop-dns(){
-	kubectl delete -f $ADDONS_DIR/dns/dns-rc.yaml
-	kubectl delete -f $ADDONS_DIR/dns/dns-svc.yaml
-}
-
-start-registry(){
-	kubectl create -f $ADDONS_DIR/registry/registry-rc.yaml
-	kubectl create -f $ADDONS_DIR/registry/registry-svc.yaml
-}
-stop-registry(){
-	kubectl delete -f $ADDONS_DIR/registry/registry-rc.yaml
-	kubectl delete -f $ADDONS_DIR/registry/registry-svc.yaml
-}
 
 disable(){
 	systemctl daemon-reload
@@ -373,26 +426,15 @@ disable(){
 	systemctl stop flannel etcd k8s-master k8s-worker
 	systemctl disable flannel etcd k8s-master k8s-worker
 	
-	dropins-clean
+	dropins-enable-overlay
 
 	systemctl restart docker
 }
 
-checkformaster(){
 
-	# Is the config file there? Then source it
-	if [[ -f $KUBERNETES_CONFIG ]]; then
-		source $KUBERNETES_CONFIG
-
-		# If ping doesn't return unknown, its OK
-		if [[ -z $(ping -c1 $K8S_MASTER_IP | grep unknown) ]]; then
-			echo "OK"
-		fi
-	fi
-}
 
 remove-etcd-datadir(){
-	read -p "Do you want to delete all Kubernetes data about this cluster? [Y/n]" removeanswer
+	read -p "Do you want to delete all Kubernetes data about this cluster? n is default [Y/n] " removeanswer
 	case $removeanswer in
 		[yY]*)
 			rm -r /var/lib/etcd
@@ -421,15 +463,18 @@ version(){
     fi
 }
 
-
-if [[ -z $1 ]]; then
+# If nothing is specified, return usage
+if [[ $# == 0 ]]; then
         usage
         exit
 fi
 
+# Commands available
 case $1 in
         'install')
                 install;;
+
+
         'build')
 				shift
 				build $@;;
@@ -437,16 +482,24 @@ case $1 in
                 build ${REQUIRED_MASTER_IMAGES[@]};;
         'build-addons')
 				build ${REQUIRED_ADDON_IMAGES[@]};;
+
+
         'enable-master')
                 start-master;;
         'enable-worker')
                 start-worker;;
         'enable-addon')
-				start $2;;
-		'disable-machine')
+				start-addon $2;;
+
+
+		'disable-node')
+				disable;;
+		'disable')
 				disable;;
 		'disable-addon')
-				stop $2;;
+				stop-addon $2;;
+
+
        	'delete-data')
 				remove-etcd-datadir;;
         'info')
