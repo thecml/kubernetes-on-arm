5474bfbe2f3d76f1479f85b686818553abbd81a7
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2015-10-27 16:38:34 +0200

    Fixed a small issue if the sd card build data doesn't exist and if docker isn't installed

--- sdcard/rootfs/kube-archlinux/usr/bin/kube-config.sh
+++ sdcard/rootfs/kube-archlinux/usr/bin/kube-config.sh
@@ -520,27 +520,28 @@ version(){
 	echo "Free disk space: $(df -h | grep /dev/root | awk '{print $4}')B ($(df | grep /dev/root | awk '{print $4}') KB)"
 	echo
 
-	source /etc/kubernetes/sdcard_build_date.conf
-	D=$SDCARD_BUILD_DATE
-	echo "SD Card was built: $(echo $D | cut -c1-2)-$(echo $D | cut -c3-4)-20$(echo $D | cut -c5-6) $(echo $D | cut -c8-9):$(echo $D | cut -c10-11)"
-	echo
-	echo "kubernetes-on-arm: "
-	echo "Latest commit: $K8S_ON_ARM_COMMIT"
-	echo "Version: $K8S_ON_ARM_VERSION"
-	echo
+	if [[ -f /etc/kubernetes/sdcard_build_date.conf ]]; then
+		source /etc/kubernetes/sdcard_build_date.conf
+		D=$SDCARD_BUILD_DATE
+		echo "SD Card was built: $(echo $D | cut -c1-2)-$(echo $D | cut -c3-4)-20$(echo $D | cut -c5-6) $(echo $D | cut -c8-9):$(echo $D | cut -c10-11)"
+		echo
+		echo "kubernetes-on-arm: "
+		echo "Latest commit: $K8S_ON_ARM_COMMIT"
+		echo "Version: $K8S_ON_ARM_VERSION"
+		echo
+	fi
 
+	echo "systemd version: $(systemctl --version | head -1 | cut -c9-)"
 	# Is docker running?
     docker ps 2> /dev/null 1> /dev/null
     if [ "$?" == "0" ]; then
 
+    	echo "docker version: $(docker version | grep "Server version" | awk '{print $3}')"
     	# Do we have hyperkube? Then output version
       	if [[ ! -z $(docker images | grep $K8S_PREFIX/hyperkube) ]]; then
       		echo "kubernetes version: $(docker run --rm $K8S_PREFIX/hyperkube /hyperkube --version | grep -o "v[0-9\.]*" | cut -c2-)"
       	fi
     fi
-
-    echo "systemd version: $(systemctl --version | head -1 | cut -c9-)"
-    echo "docker version: $(docker version | grep "Server version" | awk '{print $3}')"
 }
 
 # If nothing is specified, return usage
