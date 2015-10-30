1f29cb44d88f84161138f11815b1e80708b1dc67
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2015-10-30 15:42:36 +0200

    Update flannel to 0.5.4, add future options as comments

--- images/version.sh
+++ images/version.sh
@@ -7,8 +7,12 @@
 # These projects are downloaded from "github.com/user/repo/archive/$VERSION.tar.gz"
 K8S_VERSION='v1.0.6'
 ETCD_VERSION='v2.0.12'
-FLANNEL_VERSION='v0.5.3'
+FLANNEL_VERSION='v0.5.4'
 REGISTRY_VERSION='v2.1.1'
 
 ## OTHER PROJECTS ##
 GO_VERSION='1.4.2'
+
+# New go version
+#GO_BOOTSTRAP_VERSION='go1.4.2'
+#GO_VERSION='go1.5.1'
