b43bc096272761306d06e10fec8d809c3d4da69c
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2015-11-05 19:01:44 +0200

    Newer kubernetes and registry version

--- images/version.sh
+++ images/version.sh
@@ -1,14 +1,14 @@
 # Specify which versions the binaries should have
 
 # Two options, a branch like 'master' or a version tag e. g. 'v1.0.1'
-# Todo: eventually include the download paths here also
+# Todo: maybe include the download paths here also
 
 ## GITHUB PROJECTS ##
 # These projects are downloaded from "github.com/user/repo/archive/$VERSION.tar.gz"
-K8S_VERSION='v1.0.6'
+K8S_VERSION='v1.0.7'
 ETCD_VERSION='v2.2.1'
 FLANNEL_VERSION='v0.5.4'
-REGISTRY_VERSION='v2.1.1'
+REGISTRY_VERSION='v2.2.0'
 
 ## OTHER PROJECTS ##
 #GO_VERSION='go1.4.2'
