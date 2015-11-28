d0fda80b05f3599940495ebc53783f44b5176296
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2015-11-28 14:40:00 +0200

    Install docker v1.7.1 manually instead of latest from pacman

--- sdcard/rootfs/kube-archlinux/etc/kubernetes/dynamic-env/archlinux.sh
+++ sdcard/rootfs/kube-archlinux/etc/kubernetes/dynamic-env/archlinux.sh
@@ -3,8 +3,13 @@ os_install(){
 
 	# Update the system and use pacman to install all the packages
 	# The two commands may be combined, but I leave it as is for now.
-	pacman -Syu --noconfirm
-	pacman -S $@ --noconfirm --needed
+	os_upgrade
+	# pacman -S $@ --noconfirm --needed
+
+	# Install this manually, docker v1.7.1
+	pacman -S bridge-utils iproute2 device-mapper sqlite git
+	curl -sSL https://s3.amazonaws.com/docker-armv7/docker-1:1.7.1-2-armv7h.pkg.tar.xz > /var/cache/pacman/pkg/docker-1:1.7.1-2-armv7h.pkg.tar.xz
+	pacman -U  /var/cache/pacman/pkg/docker-1:1.7.1-2-armv7h.pkg.tar.xz
 
 	# Add more commands here, archlinux specific
 }
