fc5086e525249e208d2596533d55325d4ea94693
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2015-12-05 15:12:39 +0200

    Build docker statically on armv6 source code

---
+++ scripts/build-docker-statically/README.md
@@ -0,0 +1,4 @@
+#### Build docker statically on armv6
+
+
+This should run on a Raspberry Pi 1
---
+++ scripts/build-docker-statically/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+DOCKERFILEDIR="$( dirname "${BASH_SOURCE[0]}" )"
+
+DOCKER_BRANCH=${DOCKER_BRANCH:-"v1.8.3"}
+
+# Download docker
+git clone -b $DOCKER_BRANCH --single-branch https://github.com/docker/docker.git ~/docker
+cd ~/docker
+
+# Patch docker
+mv vendor/src/github.com/opencontainers/runc/libcontainer/seccomp/jump_{amd64,linux}.go
+sed -i 's/,amd64//' vendor/src/github.com/opencontainers/runc/libcontainer/seccomp/jump_linux.go
+
+# Copy the Dockerfile from kubernetes-on-arm source to docker build dir
+cp -f $DOCKERFILEDIR/Dockerfile .
+
+# Customize the dockerfile for armv6 and 1.8.x => start with a 1.8.2 umiddleb/armhf Dockerfile
+#curl -sSL https://raw.githubusercontent.com/umiddelb/armhf/ed1d3b1dcd6bd4112b2183c93996a4d4379aed7c/Dockerfile.armv7 > Dockerfile
+
+# Change GOARM and base image. armv7 doesn't run on armv6
+#sed -e "s@GOARM 7@GOARM 6@" -i Dockerfile
+#sed -e "s@armv7/armhf-ubuntu:14.04@resin/rpi-raspbian:jessie@" -i Dockerfile
+
+# Comment out these lines
+#sed -e "s@s3cmd=@#s3cmd=@" -i Dockerfile
+#sed -e "s@RUN gem install --no-rdoc --no-ri fpm@#RUN gem install --no-rdoc --no-ri fpm@" -i Dockerfile
+
+# Install make if needed
+if [[ -f $(which pacman) ]]; then
+	pacman -S make --noconfirm --needed
+fi
+
+# The actual build
+time make binary
