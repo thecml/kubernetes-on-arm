f3e50c0505d2ff150b917c3fc1fe5fdfabeac42d
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2015-12-10 22:28:19 +0200

    Bump to newer versions. Test to use go1.5.2, which could be useful when running on amd64 via qemu

--- images/version.sh
+++ images/version.sh
@@ -5,17 +5,17 @@
 
 ## GITHUB PROJECTS ##
 # These projects are downloaded from "github.com/user/repo/archive/$VERSION.tar.gz"
-K8S_VERSION='v1.1.2'
-ETCD_VERSION='v2.2.1'
-FLANNEL_VERSION='v0.5.4'
-REGISTRY_VERSION='v2.2.0'
+K8S_VERSION='v1.1.3'
+ETCD_VERSION='v2.2.2'
+FLANNEL_VERSION='v0.5.5'
+REGISTRY_VERSION='v2.2.1'
 
 ## OTHER PROJECTS ##
 # For the moment, go1.4.3 is a better choice than go1.5.1
 # Because building with go1.5.1 is 2-4x slower than 1.4.3
 # And there's no big difference in the result
-GO_VERSION='go1.4.3'
+# GO_VERSION='go1.4.3'
 
 # Enable go1.5.1 by commenting above and uncommenting here
-#GO_BOOTSTRAP_VERSION='go1.4.3'
-#GO_VERSION='go1.5.1'
+GO_BOOTSTRAP_VERSION='go1.4.3'
+GO_VERSION='go1.5.2'
