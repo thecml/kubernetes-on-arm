ce5419193610fe9bd63661810c7ddab599343c1e
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2015-12-14 23:31:03 +0200

    Fix parallella variable bug

--- sdcard/rootfs/kube-systemd/etc/kubernetes/dynamic-env/parallella.sh
+++ sdcard/rootfs/kube-systemd/etc/kubernetes/dynamic-env/parallella.sh
@@ -2,6 +2,6 @@
 post_install(){
 
 	# Parallella patch, specific to this rootfs. Disable overlay, because linux 3.14 doesn't have overlay support
-	sed -e "s@-s overlay@@" -i $K8S_DIR/dropins/docker-flannel.conf
-	sed -e "s@-s overlay@@" -i $K8S_DIR/dropins/docker-overlay.conf
+	sed -e "s@-s overlay@@" -i $KUBERNETES_DIR/dropins/docker-flannel.conf
+	sed -e "s@-s overlay@@" -i $KUBERNETES_DIR/dropins/docker-overlay.conf
 }
