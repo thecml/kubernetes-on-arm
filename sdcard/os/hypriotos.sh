9fac51afe39d373579b6af40909ca4dc7d785751
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2016-02-12 13:43:35 +0200

    Add initial experimental support for odroid c1

--- sdcard/os/hypriotos.sh
+++ sdcard/os/hypriotos.sh
@@ -1,5 +1,13 @@
 
 
+RPI_HYPRIOTOS_RELEASE="hypriot-rpi-20151115-132854"
+RPI_DOWNLOAD_LINK=http://downloads.hypriot.com/${RPI_HYPRIOTOS_RELEASE}.img.zip
+
+ODROID_C1_HYPRIOTOS_VERSION="v0.2.1"
+ODROID_C1_HYPRIOTOS_RELEASE="sd-card-odroid-c1-${ODROID_C1_HYPRIOTOS_VERSION}"
+ODROID_C1_DOWNLOAD_LINK=https://github.com/hypriot/image-builder-odroid-c1/releases/download/${ODROID_C1_HYPRIOTOS_VERSION}/${ODROID_C1_HYPRIOTOS_RELEASE}.img.zip
+
+
 # First invoked by sdcard/write
 mountpartitions(){
 	# Partition the sd card
@@ -11,7 +19,9 @@ mountpartitions(){
 initos(){
 	case $MACHINENAME in
 		rpi|rpi-2)
-			generaldownload;;
+			generaldownload $RPI_DOWNLOAD_LINK $RPI_HYPRIOTOS_RELEASE;;
+		odroid-c1)
+			generaldownload $ODROID_C1_DOWNLOAD_LINK $ODROID_C1_HYPRIOTOS_RELEASE;;
 		*)
 			exit;;
 	esac
@@ -20,30 +30,36 @@ initos(){
 # Invoked by sdcard/write
 cleanup(){
 	case $MACHINENAME in
-		rpi|rpi-2)
+		rpi|rpi-2|odroid-c1)
 			umount_root;;
 		*)
 			exit;;
 	esac
 }
 
-HYPRIOTOS_RELEASE="20151115-132854"
 
+# Takes an URL (.img.zip file) to download an the name of the downloaded file. Assumes that the extracted and the downloaded file has the same names except for the extension
 generaldownload(){
 
 	# Install unzip if not present
 	require unzip unzip
 
+	# Ensure this page is present
 	mkdir -p /etc/tmp
+
 	# We can't write this .img file to /tmp because /tmp has a limit of 462MB
 	DLDIR=/etc/tmp/downloadhypriot
-	if [[ ! -f $DLDIR/hypriotos-${HYPRIOTOS_RELEASE}.img.zip ]]; then
-		curl -sSL http://downloads.hypriot.com/hypriot-rpi-${HYPRIOTOS_RELEASE}.img.zip > $DLDIR/hypriotos-${HYPRIOTOS_RELEASE}.img.zip
+	DL_LINK=$1
+	RELEASE=$2
+	ZIPFILE=$DLDIR/${RELEASE}.img.zip
+
+	if [[ ! -f $ZIPFILE ]]; then
+		curl -sSL $DL_LINK > $ZIPFILE
 	fi
 
-	unzip $DLDIR/hypriotos-${HYPRIOTOS_RELEASE}.img.zip -d $DLDIR
+	unzip $ZIPFILE -d $DLDIR
 
-	dd if=$(ls $DLDIR/*.img) of=$SDCARD bs=4M
+	dd if=$DLDIR/${RELEASE}.img of=$SDCARD bs=4M
 
 	sync
 
