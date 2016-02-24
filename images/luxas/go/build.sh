36d7ae81e5359c69d5047007d2060b559d0e172c
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2016-02-24 07:48:39 +0200

    Make the go image use prebuilt go binaries

--- images/luxas/go/build.sh
+++ images/luxas/go/build.sh
@@ -5,18 +5,3 @@ cp ../../version.sh .
 
 # Build the image
 docker build -t luxas/go .
-
-# Make the filesystem
-CID=$(docker run -d luxas/go /bin/bash)
-
-# Get go and gofmt
-docker cp $CID:/goroot/bin .
-
-# Kanske inte alltid finns
-rm -rf _bin
-
-# Rename the directory to the standard _bin
-mv bin _bin
-
-# Remove the intermediate image
-docker rm $CID
