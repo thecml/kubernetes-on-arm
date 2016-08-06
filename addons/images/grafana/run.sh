8d67a6d63333a173b76b8d3cb0d8c14930cafe4f
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2016-08-06 22:03:27 +0300

    Refactor the grafana run.sh script and add curl to the image

--- addons/images/grafana/run.sh
+++ addons/images/grafana/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 HEADER_CONTENT_TYPE="Content-Type: application/json"
 HEADER_ACCEPT="Accept: application/json"
@@ -30,47 +30,45 @@ fi
 echo "Using the following URL for InfluxDB: ${INFLUXDB_SERVICE_URL}"
 echo "Using the following backend access mode for InfluxDB: ${BACKEND_ACCESS_MODE}"
 
-set -m
-echo "Starting Grafana in the background"
-exec /usr/sbin/grafana-server --homepath=/usr/share/grafana --config=/etc/grafana/grafana.ini cfg:default.paths.data=/var/lib/grafana cfg:default.paths.logs=/var/log/grafana &
 
-echo "Waiting for Grafana to come up..."
-GRAFANA_SECONDS=0
-while [[ $(curl --fail --output /dev/null --silent http://${GRAFANA_USER}:${GRAFANA_PASSWD}@localhost:${GRAFANA_PORT}/api/org; echo $?) != 0 ]]; do
-  
-  ((GRAFANA_SECONDS++))
-  # Wait max 40 seconds
-  if [[ ${GRAFANA_SECONDS} == 20 ]]; then
-    echo "grafana failed to start. Exiting..." 2>&1
-    exit
-  fi
+(
+  echo "Waiting for Grafana to come up..."
+  GRAFANA_SECONDS=0
+  while [ $(curl --fail --output /dev/null --silent http://${GRAFANA_USER}:${GRAFANA_PASSWD}@localhost:${GRAFANA_PORT}/api/org; echo $?) != 0 ]; do
 
+    $((GRAFANA_SECONDS++))
+    # Wait max 40 seconds
+    if [[ ${GRAFANA_SECONDS} == 40 ]]; then
+      echo "grafana failed to start. Exiting..." 2>&1
+      exit
+    fi
 
-  printf "."
-  sleep 2
-done
-echo "Grafana is up and running."
-echo "Creating default influxdb datasource..."
-curl -i -XPOST -H "${HEADER_ACCEPT}" -H "${HEADER_CONTENT_TYPE}" "http://${GRAFANA_USER}:${GRAFANA_PASSWD}@localhost:${GRAFANA_PORT}/api/datasources" -d '
-{
-  "name": "influxdb-datasource",
-  "type": "influxdb",
-  "access": "'"${BACKEND_ACCESS_MODE}"'",
-  "isDefault": true,
-  "url": "'"${INFLUXDB_SERVICE_URL}"'",
-  "password": "'"${INFLUXDB_PASSWORD}"'",
-  "user": "'"${INFLUXDB_USER}"'",
-  "database": "'"${INFLUXDB_DATABASE}"'"
-}'
+    printf "."
+    sleep 2
+  done
+  echo "Grafana is up and running."
+  echo "Creating default influxdb datasource..."
+  curl -i -XPOST -H "${HEADER_ACCEPT}" -H "${HEADER_CONTENT_TYPE}" "http://${GRAFANA_USER}:${GRAFANA_PASSWD}@localhost:${GRAFANA_PORT}/api/datasources" -d '
+  {
+    "name": "influxdb-datasource",
+    "type": "influxdb",
+    "access": "'"${BACKEND_ACCESS_MODE}"'",
+    "isDefault": true,
+    "url": "'"${INFLUXDB_SERVICE_URL}"'",
+    "password": "'"${INFLUXDB_PASSWORD}"'",
+    "user": "'"${INFLUXDB_USER}"'",
+    "database": "'"${INFLUXDB_DATABASE}"'"
+  }'
 
-echo ""
-echo "Importing default dashboards..."
-for filename in ${DASHBOARD_LOCATION}/*.json; do
-  echo "Importing ${filename} ..."
-  curl -i -XPOST --data "@${filename}" -H "${HEADER_ACCEPT}" -H "${HEADER_CONTENT_TYPE}" "http://${GRAFANA_USER}:${GRAFANA_PASSWD}@localhost:${GRAFANA_PORT}/api/dashboards/db"
   echo ""
-  echo "Done importing ${filename}"
-done
-echo ""
-echo "Bringing Grafana back to the foreground"
-fg
+  echo "Importing default dashboards..."
+  for filename in ${DASHBOARD_LOCATION}/*.json; do
+    echo "Importing ${filename} ..."
+    curl -i -XPOST --data "@${filename}" -H "${HEADER_ACCEPT}" -H "${HEADER_CONTENT_TYPE}" "http://${GRAFANA_USER}:${GRAFANA_PASSWD}@localhost:${GRAFANA_PORT}/api/dashboards/db"
+    echo ""
+    echo "Done importing ${filename}"
+  done
+) >/dev/stdout 2>/dev/stderr &
+
+echo "Starting Grafana"
+exec /usr/sbin/grafana-server --homepath=/usr/share/grafana --config=/etc/grafana/grafana.ini cfg:default.paths.data=/var/lib/grafana cfg:default.paths.logs=/var/log/grafana
