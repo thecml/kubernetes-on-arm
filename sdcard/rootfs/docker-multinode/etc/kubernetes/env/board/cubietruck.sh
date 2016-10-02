c2f697ce14377f8f3c8771526b0f18b1278bce95
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2016-10-02 18:34:57 +0300

    Delete now unsupported boards

--- sdcard/rootfs/docker-multinode/etc/kubernetes/env/board/cubietruck.sh
+++
@@ -1,6 +0,0 @@
-# This command is run by kube-config when doing "kube-config install"
-board_post_install(){
-
-    # Install the cubietruck uboot package
-    pacman -S uboot-cubietruck --noconfirm
-}
--- sdcard/rootfs/docker-multinode/etc/kubernetes/env/board/parallella.sh
+++
@@ -1,4 +0,0 @@
-# This command is run by kube-config when doing "kube-config install"
-board_install(){
-    export INSTALL_STORAGE_DRIVER=devicemapper
-}
